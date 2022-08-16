fd2bce64af7b46252ca8bde73c5c860c9b82c0ab
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-08-16 21:22:53 +1000

    update

--- src/gql/index.tsx
+++ src/gql/index.tsx
@@ -7,7 +7,7 @@ query Proposals($name:String) {
     skip: 0,
     where: {
       space_in: ["balancer", $name],
-      state: "pending"
+      state: "active"
     },
     orderBy: "created",
     orderDirection: desc
