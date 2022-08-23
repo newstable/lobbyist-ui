60cc2ebb3ecd59d5d1926f09ae6c004d611713c4
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-08-23 14:36:04 +1000

    update

--- src/common/number.ts
+++ src/common/number.ts
@@ -2,11 +2,10 @@ const NumberType = (data: String) => {
     console.log("number", data);
     var str = "";
     for (var i = 0; i < Math.floor(data.length / 3); i++) {
+        console.log(data.slice(-(i + 1) * 3, -i * 3));
         str = "," + data.slice(-(i + 1) * 3, -i * 3) + str;
     }
-    console.log("return", str);
     str = data.slice(-(Math.floor(data.length / 3) + 1) * 3, -Math.floor(data.length / 3) * 3) + str;
-    console.log("return", str);
     return str;
 }
 
