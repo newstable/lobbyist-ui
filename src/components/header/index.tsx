be694c887d60127b776cebb63babcb570899281a
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-09-06 22:32:38 +1000

    updatE

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,13 +8,22 @@ import {
     ClickAwayListener,
     MenuList,
     MenuItem,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    IconButton,
+    Typography,
 } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 import { styled } from "@mui/material/styles";
 import classNames from "classnames";
 import Web3Modal from "web3modal";
 import { ethers } from "ethers";
 import { setWalletAddress } from "../../redux/slices/wallet";
 import { dispatch } from "../../redux/store";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 import { colors } from "../../common";
 import { HeaderLeft } from "./left";
@@ -45,6 +54,15 @@ const web3Modal = new Web3Modal({
     providerOptions, // required
 });
 
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
+
 const Header: FC = () => {
     const [provider, setProvider] = useState();
     const [library, setLibrary]: any = useState();
@@ -56,6 +74,7 @@ const Header: FC = () => {
     const [signedMessage, setSignedMessage] = useState("");
     const [verified, setVerified] = useState();
     const [open, setOpen] = useState(false);
+    const [walletInfo, setWalletInfo] = useState(false);
     const anchorRef = useRef<HTMLButtonElement>(null);
     const [selectedCrypto, setselectedCrypto] = useState("Polygon");
     const [selectedImg, setselectedImg] = useState("../../../../assets/chains/matic.svg");
@@ -118,36 +137,6 @@ const Header: FC = () => {
         }
     }, []);
 
-    // useEffect(() => {
-    //   if (provider?.on) {
-    //     const handleAccountsChanged = (accounts:any) => {
-    //       console.log("accountsChanged", accounts);
-    //       if (accounts) setAccount(accounts[0]);
-    //     };
-
-    //     const handleChainChanged = (_hexChainId:any) => {
-    //       setChainId(_hexChainId);
-    //     };
-
-    //     const handleDisconnect = () => {
-    //       console.log("disconnect", error);
-    //       disconnect();
-    //     };
-
-    //     provider.on("accountsChanged", handleAccountsChanged);
-    //     provider.on("chainChanged", handleChainChanged);
-    //     provider.on("disconnect", handleDisconnect);
-
-    //     return () => {
-    //       if (provider.removeListener) {
-    //         provider.removeListener("accountsChanged", handleAccountsChanged);
-    //         provider.removeListener("chainChanged", handleChainChanged);
-    //         provider.removeListener("disconnect", handleDisconnect);
-    //       }
-    //     };
-    //   }
-    // }, [provider]);
-
     const refreshState = () => {
         setAccount("");
         setMessage("");
@@ -155,11 +144,11 @@ const Header: FC = () => {
         setVerified(undefined);
     };
 
-    const disconnect = async () => {
-        await web3Modal.clearCachedProvider();
-        dispatch(setWalletAddress(""));
-        refreshState();
-    };
+    // const disconnect = async () => {
+    //     await web3Modal.clearCachedProvider();
+    //     dispatch(setWalletAddress(""));
+    //     refreshState();
+    // };
 
     const handleToggle = () => {
         setOpen((prevOpen) => !prevOpen);
@@ -202,95 +191,133 @@ const Header: FC = () => {
         prevOpen.current = open;
     }, [open]);
 
+    const handleClickOpen = () => {
+        setWalletInfo(true);
+    };
+    const onHandleModalClose = () => {
+        setWalletInfo(false);
+    };
+
     return (
-        <header
-            className={classNames(
-                "flex items-center justify-between content w-full mx-auto",
-                styles.main
-            )}
-        >
-            <HeaderLeft />
-            <Box className="hidden mlg:flex gap-6">
-                <Button
-                    variant="contained"
-                    color="third"
-                    disableRipple
-                    className="!cursor-default"
-                    component="button"
-                    ref={anchorRef}
-                    onClick={handleToggle}
-                >
-                    <img style={{ marginRight: "7px" }} width="17px" src={selectedImg}></img>
-                    {selectedCrypto}
-                </Button>
-                <Popper
-                    open={open}
-                    anchorEl={anchorRef.current}
-                    role={undefined}
-                    placement="bottom-start"
-                    transition
-                    disablePortal
-                >
-                    {({ TransitionProps, placement }) => (
-                        <Grow
-                            {...TransitionProps}
-                            style={{
-                                transformOrigin:
-                                    placement === "bottom-start"
-                                        ? "left top"
-                                        : "left bottom",
-                            }}
-                        >
-                            <StyledPaper className="min-w-15">
-                                <ClickAwayListener onClickAway={handleClose}>
-                                    <MenuList
-                                        autoFocusItem={open}
-                                        id="composition-menu"
-                                        aria-labelledby="composition-button"
-                                        onKeyDown={handleListKeyDown}
-                                    >
-                                        {itemsList.map((il, idx) => (
-                                            <MenuItem
-                                                className="!justify-center"
-                                                onClick={() =>
-                                                    selectMenuItem(
-                                                        il.name,
-                                                        il.id,
-                                                        il.img
-                                                    )
-                                                }
-                                                key={`mi_${idx}`}
-                                            >
-                                                <img width="17px" style={{ marginRight: "7px" }} src={il.img}></img>
-                                                {il.name}
-                                            </MenuItem>
-                                        ))}
-                                    </MenuList>
-                                </ClickAwayListener>
-                            </StyledPaper>
-                        </Grow>
-                    )}
-                </Popper>
-                {!account ? (
-                    <Button
-                        variant="contained"
-                        color="tealLight"
-                        onClick={connectWallet}
-                    >
-                        Connect
-                    </Button>
-                ) : (
-                    <Button
-                        variant="contained"
-                        color="tealLight"
-                        onClick={disconnect}
-                    >
-                        <img width="17px" style={{ marginRight: "7px" }} src="../../../../assets/chains/account.svg"></img>
-                        {styledAddress}
-                    </Button>
+        <>
+            <header
+                className={classNames(
+                    "flex items-center justify-between content w-full mx-auto",
+                    styles.main
                 )}
-            </Box>
-        </header>
+            >
+                <HeaderLeft />
+                <Box className="hidden mlg:flex gap-6">
+                    <Button
+                        variant="contained"
+                        color="third"
+                        disableRipple
+                        className="!cursor-default"
+                        component="button"
+                        ref={anchorRef}
+                        onClick={handleToggle}
+                    >
+                        <img style={{ marginRight: "7px" }} width="17px" src={selectedImg}></img>
+                        {selectedCrypto}
+                    </Button>
+                    <Popper
+                        open={open}
+                        anchorEl={anchorRef.current}
+                        role={undefined}
+                        placement="bottom-start"
+                        transition
+                        disablePortal
+                    >
+                        {({ TransitionProps, placement }) => (
+                            <Grow
+                                {...TransitionProps}
+                                style={{
+                                    transformOrigin:
+                                        placement === "bottom-start"
+                                            ? "left top"
+                                            : "left bottom",
+                                }}
+                            >
+                                <StyledPaper className="min-w-15">
+                                    <ClickAwayListener onClickAway={handleClose}>
+                                        <MenuList
+                                            autoFocusItem={open}
+                                            id="composition-menu"
+                                            aria-labelledby="composition-button"
+                                            onKeyDown={handleListKeyDown}
+                                        >
+                                            {itemsList.map((il, idx) => (
+                                                <MenuItem
+                                                    className="!justify-center"
+                                                    onClick={() =>
+                                                        selectMenuItem(
+                                                            il.name,
+                                                            il.id,
+                                                            il.img
+                                                        )
+                                                    }
+                                                    key={`mi_${idx}`}
+                                                >
+                                                    <img width="17px" style={{ marginRight: "7px" }} src={il.img}></img>
+                                                    {il.name}
+                                                </MenuItem>
+                                            ))}
+                                        </MenuList>
+                                    </ClickAwayListener>
+                                </StyledPaper>
+                            </Grow>
+                        )}
+                    </Popper>
+                    {!account ? (
+                        <Button
+                            variant="contained"
+                            color="tealLight"
+                            onClick={connectWallet}
+                        >
+                            Connect
+                        </Button>
+                    ) : (
+                        <Button
+                            variant="contained"
+                            color="tealLight"
+                            onClick={handleClickOpen}
+                        >
+                            <img width="17px" style={{ marginRight: "7px" }} src="../../../../assets/chains/account.svg"></img>
+                            {styledAddress}
+                        </Button>
+                    )}
+                </Box>
+            </header>
+            <BootstrapDialog
+                onClose={onHandleModalClose}
+                aria-labelledby="customized-dialog-title"
+                open={walletInfo}
+            >
+                <div className="wallet-modal-container">
+                    <div className="wallet-modal-header">
+                        <div>Account</div>
+                        <div className="close">&times;</div>
+                    </div>
+                    <div className="wallet-modal-body">
+                        <div className="justify-s w10">
+                            <div className="">Connected with Metamask</div>
+                            <div className="cursorpoint">Change</div>
+                        </div>
+                        <div className="flex justify-start w10 font-1">
+                            <div className="">0xbC7a...9051</div>
+                            <div className=""></div>
+                        </div>
+                        <div className="justify-s w10">
+                            <div className="cursorpoint"><ContentCopyIcon /> Copy Address</div>
+                            <div className="view"><OpenInNewIcon />View on Block Explorer</div>
+                        </div>
+                    </div>
+                    <div className="wallet-modal-footer">
+                        Your transactions will appear here...
+                    </div>
+                </div>
+            </BootstrapDialog>
+        </>
     );
 };
 
