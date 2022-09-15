8bafec375106ab8e53d40b401cc186cc085f56c4
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-09-15 17:08:37 +1000

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./redux/store";
@@ -11,9 +11,6 @@ import "./assets/css/nucleo-icons.css"
 import "./assets/css/app.css"
 import 'react-notifications/lib/notifications.css';
 import { NotificationContainer } from 'react-notifications';
-import Action from "./services";
-import { useSelector, RootState } from "./redux/store";
-import { NotificationManager } from 'react-notifications';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -23,27 +20,6 @@ const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPQLENDPOINT,
   cache: new InMemoryCache(),
 });
-
-const [address, setAddress] = useState("");
-const walletAddress: any = useSelector(
-  (state: RootState) => state.wallet.address
-);
-
-const AllInfo = async () => {
-  await Action.Proposal_load({ address: address });
-  NotificationManager.info("This app is currently in Beta, please use at your own risk", "Information");
-  if (!window.ethereum)
-    NotificationManager.error("Please install wallet in your device", "Error");
-}
-
-useEffect(() => {
-  setAddress(walletAddress);
-}, [walletAddress]);
-const [time, setTime] = useState<number>();
-useEffect(() => {
-  const timer = setTimeout(() => { setTime(+new Date()); AllInfo() }, 5000)
-  return () => clearTimeout(timer)
-}, [time])
 
 root.render(
   <React.StrictMode>
