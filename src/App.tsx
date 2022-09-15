248cae3fb6b9b5f18d56b73118f9abffd21c1b13
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-09-15 17:00:51 +1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,21 @@ import LoadingScreen from 'react-loading-screen';
 import { useThemeMode } from "./common/themeContext";
 import { getTheme } from "./common";
 import { AppRoutes } from "./routes";
-import Action from "./services";
-import { Coins } from "./blockchain";
-import { useSelector, RootState } from "./redux/store";
-import { NotificationManager } from 'react-notifications';
+
 
 interface init {
   votes: number;
 }
 
 const App = () => {
-  const [address, setAddress] = useState("");
-  const walletAddress: any = useSelector(
-    (state: RootState) => state.wallet.address
-  );
   const [loading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
   const { darkMode } = useThemeMode();
   let theme = React.useMemo(() => {
     return getTheme(darkMode);
   }, [darkMode]);
-
-  const AllInfo = async () => {
-    Action.Proposal_load({ address: address });
-    setLoading(false);
-    if (loading) {
-      NotificationManager.info("This app is currently in Beta, please use at your own risk", "Information");
-      if (!window.ethereum)
-        NotificationManager.error("Please install wallet in your device", "Error");
-    }
-  }
-
-  useEffect(() => {
-    setAddress(walletAddress);
-  }, [walletAddress]);
-  const [time, setTime] = useState<number>();
-  useEffect(() => {
-    const timer = setTimeout(() => { setTime(+new Date()); AllInfo() }, 5000)
-    return () => clearTimeout(timer)
-  }, [time])
   return (
     <ThemeProvider theme={theme}>
       <LoadingScreen loading={loading} bgColor="#282931" spinnerColor="#3a78ff">
