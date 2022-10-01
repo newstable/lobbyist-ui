5fcd93a4be795a6b7263cca3d6aa217c27eec1d4
AIASolution <58142664+bestsolution4u@users.noreply.github.com>
2022-10-01 09:04:49 -0600

    update

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -9,7 +9,7 @@ const RPCS = {
     10: "https://op.getblock.io/mainnet/?api_key=8dbb37b0-802c-4ed0-9296-e9823ca0d8b5",
     56: "https://bsc.getblock.io/mainnet/?api_key=8dbb37b0-802c-4ed0-9296-e9823ca0d8b5",
     137: "https://matic.getblock.io/mainnet/?api_key=8dbb37b0-802c-4ed0-9296-e9823ca0d8b5",
-    250: "https://ftm.getblock.io/mainnet/?api_key=8dbb37b0-802c-4ed0-9296-e9823ca0d8b5",
+    250: "https://rpc.ftm.tools",
     42161: "https://arbitrum.getblock.io/mainnet/?api_key=8dbb37b0-802c-4ed0-9296-e9823ca0d8b5",
     43114: "https://avax.getblock.io/mainnet/ext/bc/C/rpc?api_key=8dbb37b0-802c-4ed0-9296-e9823ca0d8b5"
 };
