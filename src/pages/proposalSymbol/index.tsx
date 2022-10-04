2282e4beb3a6a87d3a012100dbc6edb80f6025dd
sasuke031026 <sasuke031026@gmail.com>
2022-10-05 02:43:20 +1000

    update

--- src/pages/proposalSymbol/index.tsx
+++ src/pages/proposalSymbol/index.tsx
@@ -12,42 +12,42 @@ type Props = {};
 const tabs: string[] = ["Proposals", "History"];
 
 const ProposalSymbol = (props: Props) => {
-  const navigate = useNavigate();
-  let { symbol } = useParams();
-  const [tabIndex, setTabIndex] = useState(0);
-  useEffect(() => {
-    var mysymbol = symbols.filter((s) => s === symbol);
-    if (mysymbol.length === 0) {
-      navigate("/");
-    }
-  })
-  const paramSymbol = symbol ?? "";
+    const navigate = useNavigate();
+    let { symbol } = useParams();
+    const [tabIndex, setTabIndex] = useState(0);
+    useEffect(() => {
+        var mysymbol = symbols.filter((s) => s === symbol);
+        if (mysymbol.length === 0) {
+            navigate("/");
+        }
+    })
+    const paramSymbol = symbol ?? "";
 
-  return (
-    <Box className="main-body flex flex-col grow">
-      <Box className="flex flex-col min-h-full main-content gap-14">
-        <Box className="flex justify-between">
-          <Tabs tabIndex={tabIndex} tabs={tabs} setTabIndex={setTabIndex} />
-          <Link to={`/proposal/new/${symbol}`}>
-            <Button
-              variant="contained"
-              color="tealLight"
-              className="h-16"
-              startIcon={<ControlPointIcon />}
-            >
-              Create Proposal
-            </Button>
-          </Link>
+    return (
+        <Box className="main-body flex flex-col grow">
+            <Box className="flex flex-col min-h-full main-content gap-14">
+                <Box className="flex justify-between">
+                    <Tabs tabIndex={tabIndex} tabs={tabs} setTabIndex={setTabIndex} />
+                    <Link to={`/proposal/new/${symbol}`}>
+                        <Button
+                            variant="contained"
+                            color="tealLight"
+                            className="h-16"
+                            startIcon={<ControlPointIcon />}
+                        >
+                            Create Proposal
+                        </Button>
+                    </Link>
+                </Box>
+                {tabIndex === 0 ? (
+                    <ActiveProposals symbol={paramSymbol} isHistory={false} />
+                ) :
+                    (
+                        <ActiveProposals symbol={paramSymbol} isHistory={true} />
+                    )}
+            </Box>
         </Box>
-        {tabIndex === 0 ? (
-          <ActiveProposals symbol={paramSymbol} isHistory={false} />
-        ) :
-          (
-            <ActiveProposals symbol={paramSymbol} isHistory={true} />
-          )}
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default ProposalSymbol;
