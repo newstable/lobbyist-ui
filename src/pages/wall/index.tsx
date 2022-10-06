49736b9ab62374bd2c7c17bcfb477d681688ad13
sasuke031026 <sasuke031026@gmail.com>
2022-10-06 23:22:05 +1000

    update

--- src/pages/wall/index.tsx
+++ src/pages/wall/index.tsx
@@ -7,45 +7,56 @@ import { Proposal } from "../../@types/proposal";
 
 const Wall = () => {
     const [sort, setSort] = useState("");
+    const [protocol, setProtocol] = useState("");
     const [sortProposal, setSortProposal] = useState<Proposal[]>([]);
     const proposalState = useSelector((state) => state.proposal);
     // @ts-ignore
     const proposals = proposalState.currentProposal.data;
     useEffect(() => {
         setProposal();
-    }, [sort, proposalState]);
+    }, [sort, proposalState, protocol]);
 
     const setProposal = () => {
         var search: Proposal[] = proposals?.filter(
             (proposal: any) => proposal.chain == sort || sort == ""
         );
-        setSortProposal(search);
+        var sortedProposal: Proposal[] = search.filter(
+            (proposal: any) => proposal.type == protocol || protocol == ""
+        )
+        setSortProposal(sortedProposal);
     }
     const selectChain = (e: any) => {
         setSort(e);
     }
+    const selectProtocol = (e: any) => {
+        setProtocol(e);
+    }
 
     return (
         <div className="wall">
-            <div className="flex items-center">
-                <div className=" text-2xl">SORT BY CHAIN : </div>
-                <select className="wall-select" onChange={(e) => selectChain(e.target.value)}>
-                    <option value={""}>All</option>
-                    <option value={1}>Ethereum</option>
-                    <option value={56}>Binance</option>
-                    <option value={137}>Polygon</option>
-                    <option value={250}>Fantom</option>
-                    <option value={10}>Optimism</option>
-                    <option value={42161}>Arbitrum</option>
-                    <option value={43114}>Arvalanche</option>
-                </select>
-                <div className="text-2xl">SORT BY Protocol : </div>
-                <select className="wall-select" onChange={(e) => selectChain(e.target.value)}>
-                    <option value={""}>All</option>
-                    <option value={"vesq"}>VESQ</option>
-                    <option value={"qidao"}>QIDAO</option>
-                    <option value={"aave"}>AAE</option>
-                </select>
+            <div className="flex items-center will-justify">
+                <div className="sm:flex items-center">
+                    <div className=" text-2xl">SORT BY CHAIN : </div>
+                    <select className="wall-select" onChange={(e) => selectChain(e.target.value)}>
+                        <option value={""}>All</option>
+                        <option value={1}>Ethereum</option>
+                        <option value={56}>Binance</option>
+                        <option value={137}>Polygon</option>
+                        <option value={250}>Fantom</option>
+                        <option value={10}>Optimism</option>
+                        <option value={42161}>Arbitrum</option>
+                        <option value={43114}>Arvalanche</option>
+                    </select>
+                </div>
+                <div className="sm:flex items-center ml-6">
+                    <div className="text-2xl">SORT BY Protocol : </div>
+                    <select className="wall-select" onChange={(e) => selectProtocol(e.target.value)}>
+                        <option value={""}>All</option>
+                        <option value={"vesq"}>VESQ</option>
+                        <option value={"qidao"}>QIDAO</option>
+                        <option value={"aave"}>AAE</option>
+                    </select>
+                </div>
             </div>
             <div className="justify-center mt-12 wall-grid-col grid gap-8">
                 {sortProposal?.map((proposal, key) => {
