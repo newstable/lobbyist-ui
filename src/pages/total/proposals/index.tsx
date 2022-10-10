72422367e1fe7730f9ec39bc38f881307f0da361
sasuke031026 <sasuke031026@gmail.com>
2022-10-10 22:19:11 +1000

    update:chains

--- src/pages/total/proposals/index.tsx
+++ src/pages/total/proposals/index.tsx
@@ -6,13 +6,14 @@ import { Proposal } from "../../../@types/proposal";
 const Proposals = () => {
     const [sort, setSort] = useState("");
     const [protocol, setProtocol] = useState("");
+    const [reward, setReward] = useState("");
     const [sortProposal, setSortProposal] = useState<Proposal[]>([]);
     const proposalState = useSelector((state) => state.proposal);
     // @ts-ignore
     const proposals = proposalState.currentProposal.data;
     useEffect(() => {
         setProposal();
-    }, [sort, proposalState, protocol]);
+    }, [sort, proposalState, protocol, reward]);
     const setProposal = () => {
         var search: Proposal[] = proposals?.filter(
             (proposal: any) => proposal.chain == sort || sort == ""
@@ -20,13 +21,15 @@ const Proposals = () => {
         var sortedProposal: Proposal[] = search?.filter(
             (proposal: any) => proposal.type == protocol || protocol == ""
         )
-        setSortProposal(sortedProposal);
-    }
-    const selectChain = (e: any) => {
-        setSort(e);
-    }
-    const selectProtocol = (e: any) => {
-        setProtocol(e);
+        var sortedByReward: Proposal[] = sortedProposal.sort((a, b) => {
+            if (reward == "max")
+                return a.reward < b.reward ? 1 : -1
+            else {
+                return a.reward > b.reward ? 1 : -1
+            }
+        });
+        console.log(sortedByReward);
+        setSortProposal(sortedByReward);
     }
     return (
         <div className="wall">
@@ -34,7 +37,7 @@ const Proposals = () => {
                 <div className="sm:flex items-center">
                     <div className=" text-2xl">SORT BY CHAIN : </div>
                     <div>
-                        <select className="wall-select" onChange={(e) => selectChain(e.target.value)}>
+                        <select className="wall-select" onChange={(e) => setSort(e.target.value)}>
                             <option value={""}>All</option>
                             <option value={1}>Ethereum</option>
                             <option value={56}>Binance</option>
@@ -48,7 +51,7 @@ const Proposals = () => {
                 </div>
                 <div className="sm:flex items-center ml-6">
                     <div className="text-2xl">SORT BY Protocol : </div>
-                    <select className="wall-select" onChange={(e) => selectProtocol(e.target.value)}>
+                    <select className="wall-select" onChange={(e) => setProtocol(e.target.value)}>
                         <option value={""}>All</option>
                         <option value={"qidao"}>QIDAO</option>
                         <option value={"aave"}>AAVE</option>
@@ -57,6 +60,13 @@ const Proposals = () => {
                         <option value={"beethovenx"}>Beethovenx</option>
                         <option value={"ribbon"}>Ribbon</option>
                         <option value={"onx"}>Onx</option>
+                    </select>
+                </div>
+                <div className="sm:flex items-center ml-6">
+                    <div className="text-2xl">SORT BY REWARD : </div>
+                    <select className="wall-select" onChange={(e) => setReward(e.target.value)}>
+                        <option value={"max"}>Max</option>
+                        <option value={"min"}>Min</option>
                     </select>
                 </div>
             </div>
